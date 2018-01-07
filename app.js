62bcea5e89840c58f89d67d8df742131147204c8
Tyler Tharp <tharpta@gmail.com>
2018-01-07 17:30:02 -0500

    I broke it

--- app.js
+++ app.js
@@ -24,34 +24,3 @@ T.post('statuses/update', {status: 'This is another test'})
     throw error;
   })
 
-
-/*
-//Favorite Something
-// Initiate your search using the above paramaters
-T.get('search/tweets', params, function(err, data, response) {
-  // If there is no error, proceed
-  if(!err){
-    // Loop through the returned tweets
-    for(let i = 0; i < data.statuses.length; i++){
-      // Get the tweet Id from the returned data
-      let id = { id: data.statuses[i].id_str }
-      // Try to Favorite the selected Tweet
-      T.post('favorites/create', id, function(err, response){
-        // If the favorite fails, log the error message
-        if(err){
-          console.log(err[0].message);
-        }
-        // If the favorite is successful, log the url of the tweet
-        else{
-          let username = response.user.screen_name;
-          let tweetId = response.id_str;
-          console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
-        }
-      });
-    }
-  } else {
-    console.log(err);
-  }
-})
-
-*/
