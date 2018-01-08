883fe2704e07bdf891bd3d3201fbc9bf88b96615
Tyler Tharp <tharpta@gmail.com>
2018-01-07 22:56:48 -0500

    Random Text and Output

--- app.js
+++ app.js
@@ -3,23 +3,24 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
+var randomnumber  = require("randomnumber");
+randomnumber.generate();
+var  randomstring = require("randomstring");
+var test =randomstring.generate(randomnumber);
 
-// Set up your search parameters
-var params = {
-  q: '#nodejs',
-  count: 10,
-  result_type: 'recent',
-  lang: 'en'
-}
 
-//This Program Will Tweet Something and then favorite some tweets
+
+
+
+
+
 
 //Tweet Something
-T.post('statuses/update', {status: 'I BROKE IT'})
-  .then(function (tweet) {
+
+T.post('statuses/update', {status:test},   function(error, tweet, response) {
+  if (!error) {
     console.log(tweet);
-  })
-  .catch(function (error) {
-    throw error;
-  })
+  }
+});
+
 
